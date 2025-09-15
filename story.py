d86b4597e8bdfd1db4aabfb77e72493d67307912
Logan Sekutowski <232259444+slogan10@users.noreply.github.com>
2025-09-15 20:06:13 +0000

    added right path defeat squirrel in combat and find out the location of his acorn stash

--- story.py
+++ story.py
@@ -15,6 +15,8 @@ def left_path():
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+    print("You accept and handily defeat him, putting him in a firm headlock, then you interrogate him for the location of his acorn stash")
+    
 
 def center_path():
     print("You walk the center path and find nothing, great job.")
